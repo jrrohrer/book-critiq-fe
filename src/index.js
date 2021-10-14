ca247a31b82a6bffede6e9051c015d3dd56a66e8
jrrohrer <jessie.rohrer@gmail.com>
2021-10-14 15:21:59 -0400

    create first reducer

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux'
+import booksReducer from './reducers/bookReducer.js';
 import './index.css';
 import App from './App';
 
 // set up the redux store
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // store is where data is stored globally, reducers tell us what to do with our store based on certain actions.
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+let store = createStore(booksReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducers/bookReducer.js
@@ -0,0 +1,3 @@
+export default function booksReducer(state = {books: []}, action) {
+  return state;
+}
