41bf5d1d1ec786ab221035d87a834a544c9955c7
jrrohrer <jessie.rohrer@gmail.com>
2021-10-14 14:53:18 -0400

    import redux packages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
 
