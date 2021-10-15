df7e0b376fa2090c48c4adbe3b6e16f21cfc4bd9
jrrohrer <jessie.rohrer@gmail.com>
2021-10-15 17:30:07 -0400

    add case for add book action to bookReducer.

--- src/reducers/bookReducer.js
+++ src/reducers/bookReducer.js
@@ -2,6 +2,8 @@ export default function booksReducer(state = {books: []}, action) {
   switch (action.type) {
     case 'FETCH_BOOKS':
       return {books: action.payload}
+    case 'ADD_BOOK':
+      return {...state, books: [...state.books, action.payload]}
     default:
       return state;
   }
