d4f53b23ec64a439e0bd7593a07d2c32c5006f71
jrrohrer <jessie.rohrer@gmail.com>
2021-10-15 17:28:18 -0400

    add action for post fetch after book create form submit

---
+++ src/actions/addBook.js
@@ -0,0 +1,19 @@
+export const addBook= (formData) => {
+
+  return (dispatch) => {
+    // thunk helping again here
+    fetch('http://localhost:3001/api/v1/books', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(book => dispatch({
+        type: 'ADD_BOOK', 
+        payload: book.data
+      }))
+  }
+}
