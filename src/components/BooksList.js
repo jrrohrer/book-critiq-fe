a95ec580245c46e480143e8a52f6a1da0593797c
jrrohrer <jessie.rohrer@gmail.com>
2021-10-15 14:45:08 -0400

    render list of books

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -5,7 +5,7 @@ import React from 'react';
 const BooksList = (props) => {
   return (
     <div>
-      {props.books.map(book => <li key={book.id}>{book.title} - {book.author}</li>)}
+      {props.books.map(book => <li key={book.id}>{book.attributes.title} - {book.attributes.author}</li>)}
     </div>
   )
 }
