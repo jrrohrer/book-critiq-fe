1fbdaca8acc984de53fb6916de889dbfd9b6d987
jrrohrer <jessie.rohrer@gmail.com>
2021-10-19 18:04:56 -0400

    fix bug where refreshing on BookShow component loses data

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -6,10 +6,14 @@ const BookShow = (props) => {
   // using the useParams() hook that comes with React Router to access the id in the URL in order to render the correct book's info in the component. Using the routerProps match didn't work here, because it didn't account for skipped ID numbers. Book id: 5 was rendering at /books/2 because it was the second book in the props array. Using useParams fixed that issue, and it accounts for deleted items in the DB. 
   const {id} = useParams();
   let book = props.books.filter(book => book.id === id)
+
+  // check if the book was found. if not, return null (allow time for the fetchBook action to run.
+  if (book.length === 0) return null;
+
   let bookData = (book && book[0].attributes);
 
   console.log(bookData)
-  
+ 
   return (
     <div>
       <h2>{bookData.title}</h2>
