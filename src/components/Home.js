184dc1421240a0c162fa861039bbec48135625b4
jrrohrer <jessie.rohrer@gmail.com>
2021-10-20 16:55:20 -0400

    set up react-bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+
 
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to BookCritiq</h1>
-        <p>Helping fellow readers choose their next book by offering honest reviews.</p>
-      </div>
+      <Container className='p-3'>
+
+          <h1 className='header'>Welcome to BookCritiq</h1>
+          <p>Helping fellow readers choose their next book by offering honest reviews.</p>
+
+      </Container>
     )
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
+import {Navbar, Nav, Container} from 'react-bootstrap';
 
 class NavBar extends React.Component {
   render() {
     return (
-      <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/books/new">Add a New Book </Link>
-        <Link to="/books"> All Books </Link>
-      </nav>
+      <>
+        <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+            <Navbar.Collapse id='responsive-navbar-nav'>
+              <Nav>
+                <Nav.Link href='/'>Home</Nav.Link>
+                <Nav.Link href='/books'>All Books</Nav.Link>
+                <Nav.Link href='/books/new'>Create New Book</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </>
+
+
+      // <nav>
+      //   <Link to="/"> Home </Link>
+      //   <Link to="/books/new">Add a New Book </Link>
+      //   <Link to="/books"> All Books </Link>
+      // </nav>
     )
   }
 }
