fd8fe2ae3e029efdabbf2d0ece345cd158b28f43
jrrohrer <jessie.rohrer@gmail.com>
2021-10-20 15:02:06 -0400

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-// import {connect} from 'react-redux';
 import BooksContainer from './containers/BooksContainer.js';
+import NavBar from './components/NavBar.js';
+
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
+        <NavBar />
         <BooksContainer />
       </div>
     )
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <Link to="/books/new">Add a New Book </Link>
+        <Link to="/books"> All Books </Link>
+      </nav>
+    )
+  }
+}
+
+export default NavBar;
