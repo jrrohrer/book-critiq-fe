6f22575025dfd41a68138b1ed6bd61bebb3a4569
jrrohrer <jessie.rohrer@gmail.com>
2021-10-21 13:02:39 -0400

    add route for create review component

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReviewCreateForm from '../components/ReviewCreateForm.js';
 import Reviews from '../components/Reviews.js';
+import {Route, Switch} from 'react-router-dom';
 
 class ReviewsContainer extends React.Component {
   render () {
     return (
       <div>
-        Reviews Container
-        <ReviewCreateForm book={this.props.book}/>
+        <Switch>
+          <Route path='/books/:id/reviews/new' render={() => <ReviewCreateForm book={this.props.book} /> } />
+        </Switch>
+        {/* <ReviewCreateForm book={this.props.book}/> */}
         <Reviews reviews={this.props.book[0].attributes.reviews}/>
       </div>
     )
