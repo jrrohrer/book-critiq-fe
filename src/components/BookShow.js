672e64b3718b2d4391ab989a0e16a8b39bfdb3ad
jrrohrer <jessie.rohrer@gmail.com>
2021-10-21 13:01:30 -0400

    add button link to create review form

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, Link} from 'react-router-dom';
 import ReviewsContainer from '../containers/ReviewsContainer.js';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Button} from 'react-bootstrap';
 
 const BookShow = (props) => {
   // using the useParams() hook that comes with React Router to access the id in the URL in order to render the correct book's info in the component. Using the routerProps match didn't work here, because it didn't account for skipped ID numbers. Book id: 5 was rendering at /books/2 because it was the second book in the props array. Using useParams fixed that issue, and it accounts for deleted items in the DB. 
@@ -25,6 +25,9 @@ const BookShow = (props) => {
           <h2 className="header text-center pt-5">{bookData.title}</h2>
           <h3 className="font-italic text-center">{bookData.author}</h3>
           <p>{bookData.description}</p>
+          <Link to={`/books/${bookData.id}/reviews/new`}>
+            <Button>Create a New Review</Button>
+          </Link>
         </Col>
       </Row> 
       <ReviewsContainer book={book} />
