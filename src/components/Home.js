f0687ac79e1aa27925285adc3f296429499b6b38
jrrohrer <jessie.rohrer@gmail.com>
2021-10-21 17:43:30 -0400

    adjust styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Container} from 'react-bootstrap'
 
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="p-5 mb-4 bg-light text-center">
+      <Container className="p-5 mt-4 bg-light text-center rounded shadow">
           <h1 className='display-5 fw-bold'>Welcome to BookCritiq</h1>
           <p className='fs-4'>Helping fellow readers choose their next book by offering honest reviews.</p>
-          <Link to='/books'><button className='btn btn-secondary btn-lg' type='button'>Find a Book To Review</button></Link>
-      </div>
+          <Link to='/books'><button className='btn btn-secondary btn-lg shadow' type='button'>Find a Book To Review</button></Link>
+      </Container>
     )
   }
 }
