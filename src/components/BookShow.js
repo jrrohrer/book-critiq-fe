0563c91c99f64df701591f2b5a7f3380a9f2e81a
jrrohrer <jessie.rohrer@gmail.com>
2021-10-20 22:36:40 -0400

    add bootstrap styling to book show page

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import ReviewsContainer from '../containers/ReviewsContainer.js';
+import {Container, Row, Col} from 'react-bootstrap';
 
 const BookShow = (props) => {
   // using the useParams() hook that comes with React Router to access the id in the URL in order to render the correct book's info in the component. Using the routerProps match didn't work here, because it didn't account for skipped ID numbers. Book id: 5 was rendering at /books/2 because it was the second book in the props array. Using useParams fixed that issue, and it accounts for deleted items in the DB. 
@@ -15,15 +16,19 @@ const BookShow = (props) => {
   console.log(bookData)
  
   return (
-    <div>
-      <h2>{bookData.title}</h2>
-      <br/>
-      <h3>{bookData.author}</h3>
-      <p>{bookData.description}</p>
-      <img src={bookData.image_url} width='200' alt={`Cover of ${bookData.title}.`} />
-
+    <Container>
+      <Row>
+        <Col md={6}>
+          <img src={bookData.image_url} width='300' alt={`Cover of ${bookData.title}.`} className='p-5 mr-auto ml-auto' style={{display: 'block'}} />
+        </Col>
+        <Col lg={true}>
+          <h2 className="header text-center pt-5">{bookData.title}</h2>
+          <h3 className="font-italic text-center">{bookData.author}</h3>
+          <p>{bookData.description}</p>
+        </Col>
+      </Row> 
       <ReviewsContainer book={book} />
-    </div>
+    </Container>
   )
 }
 
