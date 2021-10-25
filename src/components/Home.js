02664ad33f76c78ac148c95be376103a4f020fe1
jrrohrer <jessie.rohrer@gmail.com>
2021-10-25 12:57:11 -0400

    refactor Home component from class to functional

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {Container} from 'react-bootstrap'
 
-
-class Home extends React.Component {
-  render() {
-    return (
-      <Container className="p-5 mt-4 bg-light text-center rounded shadow">
-          <h1 className='display-5 fw-bold'>Welcome to BookCritiq</h1>
-          <p className='fs-4'>Helping fellow readers choose their next book by offering honest reviews.</p>
-          <Link to='/books'><button className='btn btn-secondary btn-lg shadow' type='button'>Find a Book To Review</button></Link>
-      </Container>
-    )
-  }
+const Home = () => {
+  return(
+    <Container className="p-5 mt-4 bg-light text-center rounded shadow">
+      <h1 className='display-5 fw-bold'>Welcome to BookCritiq</h1>
+      <p className='fs-4'>Helping fellow readers choose their next book by offering honest reviews.</p>
+      <Link to='/books'><button className='btn btn-secondary btn-lg shadow' type='button'>Find a Book To Review</button></Link>
+    </Container>
+  )
 }
 
 export default Home;
