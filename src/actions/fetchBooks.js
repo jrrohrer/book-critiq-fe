8b6ff65a84ba3e9750d7b623f8b208098ed5bda7
jrrohrer <jessie.rohrer@gmail.com>
2021-10-25 13:54:17 -0400

    remove unnecessary comments

--- src/actions/fetchBooks.js
+++ src/actions/fetchBooks.js
@@ -1,6 +1,4 @@
-// action creator
 export function fetchBooks() {
-  // using thunk here to dispatch the result of our fetch request asynchronously
   return (dispatch) => {
     fetch('http://localhost:3001/api/v1/books')
         .then(response => response.json())
@@ -13,5 +11,3 @@ export function fetchBooks() {
         })
       }
 }
-// payload points to books.data because of the way that the serializer sends the json through: as an object containing an array of objects
-// dispatching the new data to the reducer to pass data to the store
