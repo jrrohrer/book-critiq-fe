b1c224ffd4d17133eae5818204d56325e592bb66
jrrohrer <jessie.rohrer@gmail.com>
2021-11-01 11:04:58 -0400

    fix bug where liking one book likes them all

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -3,12 +3,13 @@ import { Button } from 'react-bootstrap';
 
 class Likes extends React.Component {
   state = {
-    likes: 0
+    likes: 0,
+    bookId: this.props.bookId
   }
 
   // pulling the likes count from local storage on mount & converting it from a string to a number, then using it to set the state
   componentDidMount = () => {
-    const stringLikes = localStorage.getItem('likes')
+    const stringLikes = localStorage.getItem('likes-' + this.state.bookId)
     const likes = parseInt(stringLikes, 10)
     if (!isNaN(likes)) {
       this.setState({
@@ -20,7 +21,7 @@ class Likes extends React.Component {
   // on update of the component, store the new value of the like count in local storage
   componentDidUpdate = (prevProps, prevState) => {
     if (prevState.likes !== this.state.likes) {
-      localStorage.setItem('likes', this.state.likes)
+      localStorage.setItem('likes-' + this.state.bookId, this.state.likes)
     }
   }
 
@@ -28,12 +29,13 @@ class Likes extends React.Component {
     this.setState({
       likes: this.state.likes + 1
     })
+    console.log(this.state.bookId)
   }
 
   render() {
     return (
       <div>
-        <Button className="shadow m-3" onClick={this.handleClick}>{this.state.likes} Likes</Button>
+        <Button className="shadow m-3" id={this.state.bookId} onClick={this.handleClick}>{this.state.likes} Likes</Button>
       </div>
     )
   }
